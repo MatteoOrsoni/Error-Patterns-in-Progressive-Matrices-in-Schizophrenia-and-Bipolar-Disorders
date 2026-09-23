2e18ee9fba4eaf2bce82a5475e1f8db4c12b47b0
MatteoOrsoni <129044339+MatteoOrsoni@users.noreply.github.com>
2026-09-23 14:59:35 +0200

    Revise working directory and add sensitivity analysis
    Updated working directory setup and added sensitivity analysis for item exclusion.

--- run_the_scripts.R
+++ run_the_scripts.R
@@ -1,11 +1,18 @@
 
 ### SETTING UP YOUR WORKING DIRECTORY
 
-setwd("C:/Users/gmgno/Desktop/matriks-error-patterns")   
+setwd("your own WD")   
 getwd()          
 list.files()     
 
-source("install_dependencies.R")   # Only the first tipe
+source("install_dependencies.R")   # Only the first time
 
 ### Running the entire pipeline
-source("run_all.R")                
+source("run_all.R")
+
+### SENSITIVITY ANALYSIS EXCLUDING ITEM 34 with different marginal distribution of distractors
+
+Sys.setenv(MATRIKS_EXCLUDE_ITEMS = "34", MATRIKS_OUTPUT_DIR = "outputs_sensitivity")
+source("R/00_setup.R")
+source("R/03_matching_and_models.R"); source("R/04_posterior_contrasts.R")
+         
